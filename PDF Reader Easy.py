326fe1b49a7f419efcab59511fbf1cb7f230f6fe
Crucio104 <alessio.curcio04@gmail.com>
2025-08-26 10:07:44 +0200

    Adding file py

--- PDF Reader Easy.py
+++ PDF Reader Easy.py
@@ -1,8 +1,25 @@
 import PyPDF2
 import re
 from docx import Document
+import customtkinter as ctk
+import os
+from tkinter import filedialog, messagebox
 
+class PDFExctractorGUI:
+     def __init__(self):
+          self.window = ctk.CTk()
+          self.window.title("PDF text Extractor")
+          self.window.geometry("600x500")
 
+          self.filepath = ""
+          self.extractedtext = ""
+
+          self.create_widgets()
+     
+     def create_widgets(self):
+            title_lable = ctk.CTkLabel(self.window, text = "PDF Text Extractor", font = ctk.CTkFont(size = 20, weight = "bold"))
+            title_lable.pack(pady=20)
+            
 
 def text_extractor(path_pdf : str):
     text = ""
@@ -30,7 +47,6 @@ def text_cleaner(text : str) -> str:
         text = re.sub(r'[\x00-\x08\x0b-\x0c\x0e-\x1f]', '', text)
         text = text.replace('\x00', '')  # Byte NULL
         text = text.replace('\uffff', '')  # Unicode replacement character
-    
         return text
           
 
@@ -38,10 +54,9 @@ if __name__ == "__main__":
      path_pdf = "/home/crucio/Downloads/Calc unit 1 January.pdf"
      text = text_extractor(path_pdf)
      text = text_cleaner(text)
-     text_saver_docx(text)
      try:
         text_saver_txt(text)
-        
+        text_saver_docx(text)
      except:
           print("Couldn't save the file.")
      print(text)
